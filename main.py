36bea04aaa346de9678f278a980b007e93569f10
faylxen <faycal.benaissa02@gmail.com>
2024-11-25 20:43:33 +0100

    version 1

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Import the required modules
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import json
@@ -21,27 +23,38 @@ def transcribe(url):
 
 def click_checkbox(driver):
     driver.switch_to.default_content()
+    time.sleep(3)	
     driver.switch_to.frame(driver.find_element(By.XPATH, ".//iframe[@title='reCAPTCHA']"))
+    time.sleep(3)
     driver.find_element(By.ID, "recaptcha-anchor-label").click()
+    time.sleep(3)
     driver.switch_to.default_content()
 
 def request_audio_version(driver):
     driver.switch_to.default_content()
-    driver.switch_to.frame(driver.find_element(By.XPATH, ".//iframe[@title='recaptcha challenge expires in two minutes']"))
+    time.sleep(3)
+    driver.switch_to.frame(driver.find_element(By.XPATH, ".//iframe[@title='Le test reCAPTCHA expire dans deux minutes']"))
+    time.sleep(3)
     driver.find_element(By.ID, "recaptcha-audio-button").click()
 
 def solve_audio_captcha(driver):
+    time.sleep(3)
     text = transcribe(driver.find_element(By.ID, "audio-source").get_attribute('src'))
+    time.sleep(3)
     driver.find_element(By.ID, "audio-response").send_keys(text)
+    time.sleep(3)
     driver.find_element(By.ID, "recaptcha-verify-button").click()
 
 if __name__ == "__main__":
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-  
+    #driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    options = Options()
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service,options = options)	
     driver.get("https://www.google.com/recaptcha/api2/demo")
+    time.sleep(3)
     click_checkbox(driver)
-    time.sleep(1)
+    time.sleep(3)
     request_audio_version(driver)
-    time.sleep(1)
+    time.sleep(3)
     solve_audio_captcha(driver)
     time.sleep(10)
